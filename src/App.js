3580931eca4f5db7fadc775686e37c5ebfc07faa
thamaragerigr <thamaragerigr@gmail.com>
2020-06-07 00:40:43 +0200

    added state to input

--- src/App.js
+++ src/App.js
@@ -2,37 +2,51 @@ import React from "react";
 import Badge from "react-bootstrap/Badge";
 
 export default class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: "",
+    };
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
+
   render() {
-    const inputStyle = {
+    var inputStyle = {
       width: "400px",
       height: "50vh",
       marginLeft: "auto",
       marginRight: "auto",
-      padding:"10px"
+      padding: "10px",
     };
-    const outputStyle = {
+    var outputStyle = {
       width: "400px",
       height: "50vh",
       backgroundColor: "#DCDCDC",
       marginLeft: "auto",
       marginRight: "auto",
-      padding:"10px"
+      padding: "10px",
     };
+
     return (
       <div className="App">
         <div className="container">
           <div className="row mt-4">
             <div className="col text-center">
               <h1>
+                {" "}
                 <Badge className="text-align-center" variant="light">
                   Markdown Previewer
                 </Badge>
               </h1>
             </div>
           </div>
+
           <div className="row mt-4">
             <div className="col-md-6">
+              {" "}
               <div className="col text-center">
                 <h4>
                   <Badge className="text-align-center" variant="secondary">
@@ -40,15 +54,23 @@ export default class App extends React.Component {
                   </Badge>
                 </h4>
               </div>
-              <div className="mark-input" style={inputStyle}>
+              <div className="input" style={inputStyle}>
                 <textarea
                   className="input"
-                  style={inputStyle}>
+                  style={inputStyle}
+                  value={this.state.markdown}
+                  onChange={(e) => {
+                    this.updateMarkdown(e.target.value);
+                  }}
+                >
+                  {" "}
+                  {console.log(this.state.markdown)}
                 </textarea>
               </div>
             </div>
 
             <div className="col-md-6">
+              {" "}
               <div className="col text-center">
                 <h4>
                   <Badge className="text-align-center" variant="secondary">
@@ -56,8 +78,7 @@ export default class App extends React.Component {
                   </Badge>
                 </h4>
               </div>
-              <div style={outputStyle}>
-              </div>
+              <div style={outputStyle}></div>
             </div>
           </div>
         </div>
